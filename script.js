bd04ccabebb88b14aa7503d5c957bd0a880dcccd
zhiqiangjiang <jiangzzz@foxmail.com>
2024-02-21 22:19:40 -0700

    zoom

--- script.js
+++ script.js
@@ -193,11 +193,11 @@ function resetFields() {
 //     tileSize: 512,
 //     zoomOffset: -1
 // }).addTo(map);
-var map = L.map('map').setView([51.032077, -114.052983], 7);
+var map = L.map('map').setView([51.032077, -114.052983], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-map.setZoom(13);
+map.setZoom(18);
 
 // Icons
 var greenIcon = new L.Icon({
