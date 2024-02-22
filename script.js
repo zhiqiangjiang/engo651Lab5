7178a98b143f79e574e6b9b954ff08da8fff691d
zhiqiangjiang <jiangzzz@foxmail.com>
2024-02-21 22:27:35 -0700

    console

--- script.js
+++ script.js
@@ -198,6 +198,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 map.setZoom(50);
+console.log(map.getZoom());
 
 // Icons
 var greenIcon = new L.Icon({
