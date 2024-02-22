1d593386686ff39c2440abdbdfaa91dc6350c0ba
zhiqiangjiang <jiangzzz@foxmail.com>
2024-02-21 22:05:27 -0700

    fix map bug

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ not finished, need test
 To observe the map updates in real-time, enter matching names in the map settings section corresponding to your subscription topic.
  This lab project focuses on implementing MQTT protocol in a simple IoT Geoweb app
 - This project uses javascript geolocation API to turn smartphones into an IoT sensor
-- The webpage (https://mj-shi.github.io/ENGO651-Lab4/) allows the user to connect, subscribe to a topic, publish a message to a topic, and share their location to a topic
+- The webpage (https://zhiqiangjiang.github.io/engo651Lab5/) allows the user to connect, subscribe to a topic, publish a message to a topic, and share their location to a topic
 - The webpage has text boxes, input textboxes, buttons, and a leaflet map
 
 # Code Files
@@ -12,7 +12,7 @@ To observe the map updates in real-time, enter matching names in the map setting
 # TESTING INSTRUCTIONS
 - Use host: test.mosquitto.org and port: 8081
 - MQTTX settings - Host:mqtt://test.mosquitto.org, Port:1883
-- Go to website https://mj-shi.github.io/ENGO651-Lab4/ 
+- Go to website https://zhiqiangjiang.github.io/engo651Lab5/
     - (iOS: Edge works, Safari has map issues, Chrome not tested)
     - (PC: Edge works, Chrome works)
 - Subscribe to the topic course_name/your_name/my_temperature before clicking share status
--- script.js
+++ script.js
@@ -183,15 +183,19 @@ function resetFields() {
 }
 
 // Create map
-var map = L.map('map').setView([51.049999, -114.066666], 10);
+// var map = L.map('map').setView([51.049999, -114.066666], 10);
 
-var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1
+// var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+//     maxZoom: 18,
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+//         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1
+// }).addTo(map);
+var map = L.map('map').setView([51.032077, -114.052983], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // Icons
